097bd92a0370f1154bd7ad9f2e74647fce8c29ce
Leonardo Savio <leosavio@leosavio.com>
2023-09-25 18:40:47 -0300

    Update workers_oc.py

--- python/ocp/workers_oc.py
+++ python/ocp/workers_oc.py
@@ -1,6 +1,20 @@
 import subprocess
 import json
 
+def get_machineset_from_node(node_name):
+    # Get node details
+    node_output = subprocess.check_output(['oc', 'get', 'node', node_name, '-o', 'json'])
+    node = json.loads(node_output)
+    
+    # Extract machine name from node annotations
+    machine_annotation = 'machine.openshift.io/machine'
+    machine_name = node['metadata']['annotations'].get(machine_annotation, '').split('/')[-1]
+    
+    # Extract machineset from machine name
+    machineset_name = '-'.join(machine_name.split('-')[:-1])
+    
+    return machineset_name
+
 def get_pods_on_nodes(namespace):
     # Get list of deployments in the namespace
     deployments_output = subprocess.check_output(['oc', 'get', 'deploy', '-n', namespace, '-o', 'json'])
@@ -29,6 +43,7 @@ namespace = 'your-namespace'
 pods_on_nodes = get_pods_on_nodes(namespace)
 
 for node, pods in pods_on_nodes.items():
-    print(f"Node: {node}")
+    machineset = get_machineset_from_node(node)
+    print(f"Node: {node} - MachineSet: {machineset}")
     for pod in pods:
         print(f"  - {pod}")
