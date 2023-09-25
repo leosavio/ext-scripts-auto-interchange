1c8e8079b686ce8b81dcec846e5f4e30bc6718a5
Leonardo Savio <leosavio@leosavio.com>
2023-09-25 18:03:09 -0300

    Update workers_of_namespace.py

--- python/ocp/workers_of_namespace.py
+++ python/ocp/workers_of_namespace.py
@@ -1,28 +1,28 @@
 import openshift
 
-# Connect to OpenShift API 
-api = openshift.client.ApiClient()
+# Initialize API client
+api = openshift.OapiApi() 
 
-# Get deployments
+# Get deployments in namespace
 deployments = api.list_deployment_for_all_namespaces(namespace='mynamespace')
 
 # Track unique workers
-workers = [] 
+workers = []
 
 # Loop through deployments
 for deployment in deployments:
 
-  # Get pods
-  pods = deployment.pods
-  
+  # Get pods for deployment
+  pods = api.list_namespaced_pod(namespace='mynamespace', label_selector=f'deployment={deployment.metadata.name}')
+
   # Loop through pods
-  for pod in pods:
-  
-    # Check if worker is already in list
-    if pod.node not in workers:
+  for pod in pods.items:
+
+    # Check if worker is already tracked
+    if pod.spec.node_name not in workers:
     
-      # Add worker to list
-      workers.append(pod.node)
+      # Add new worker to list
+      workers.append(pod.spec.node_name) 
       
-# Print unique workers      
+# Print unique workers
 print(workers)
