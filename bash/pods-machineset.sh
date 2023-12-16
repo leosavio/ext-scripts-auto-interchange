50279f9dc0b82c008f860b96e8680ade8638ae3f
Leonardo Savio <leosavio@leosavio.com>
2023-12-16 00:37:50 -0300

    Update pods-machineset.sh

--- bash/pods-machineset.sh
+++ bash/pods-machineset.sh
@@ -3,8 +3,9 @@
 # Get all pods and their nodes
 pods_and_nodes=$(oc get pods -o=custom-columns=NAME:.metadata.name,NODE:.spec.nodeName --no-headers)
 
-# Iterate over each pod and its node
 echo "Pod,Node,Machine,MachineSet"
+
+# Iterate over each pod and its node
 while IFS= read -r line; do
     pod_name=$(echo $line | awk '{print $1}')
     node_name=$(echo $line | awk '{print $2}')
@@ -12,9 +13,9 @@ while IFS= read -r line; do
     # Find the corresponding machine for the node
     machine_name=$(oc get machine -o=custom-columns=NAME:.metadata.name,NODE:.status.nodeRef.name --no-headers | grep $node_name | awk '{print $1}')
 
-    # Find the machine set for the machine
-    machine_set_name=$(oc get machineset -o=custom-columns=NAME:.metadata.name,MACHINE:.spec.template.metadata.labels --no-headers | grep $machine_name | awk '{print $1}')
+    # Assuming machines are labeled with their machine set's name
+    machine_set_label=$(oc get machine $machine_name -o jsonpath='{.metadata.labels.machine\.openshift\.io/cluster-api-machineset}')
 
     # Print the pod, node, machine, and machine set
-    echo "$pod_name,$node_name,$machine_name,$machine_set_name"
+    echo "$pod_name,$node_name,$machine_name,$machine_set_label"
 done <<< "$pods_and_nodes"
